b3e43f23f56bd0105fb36c4fc3b783ae432d2b55
jchasehuskey <101059291+jchasehuskey@users.noreply.github.com>
2022-12-11 15:56:40 -0700

    created state for events and locations

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 
-function App() {
+class App extends Component {
+  state = {
+    events: [],
+    locations:[]
+  };
+
+  render(){
+  
   return (
     <div className="App">
-      <CitySearch/>
-      <EventList
+      <CitySearch locations={this.state.locations} />
+      <EventList events={this.state.events}
       />
        <NumberOfEvents />
     </div>
   );
+}
 }
 
 
--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -2,12 +2,14 @@
 
 import React from 'react';
 //renders your react components for testing, renders them without a DOM and w/out any children
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 //you cannot test a component without importing it
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
 import NumberOfEvents from '../NumberOfEvents';
+import { mockData } from '../mock-data';
+import { extractLocations } from '../api';
 
 describe('<App /> component', () => {
 
@@ -25,13 +27,33 @@ describe('<App /> component', () => {
     })
 
 
-    //task
-
     test('render NumberOfEvents', () => {
         expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
       });
 
 
+});   
+
+describe('<App /> integration', () => {
+    test('App passes "events" state as a prop to EventList', () => {
+      const AppWrapper = mount(<App />);
+      const AppEventsState = AppWrapper.state('events');
+      expect(AppEventsState).not.toEqual(undefined);
+      expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState);
+      AppWrapper.unmount();
+    });
 
 
-});                      
+    test('App passes "locations" state as a prop to CitySearch', () => {
+        const AppWrapper = mount(<App />);
+        const AppLocationsState = AppWrapper.state('locations');
+        expect(AppLocationsState).not.toEqual(undefined);
+        expect(AppWrapper.find(CitySearch).props().locations).toEqual(AppLocationsState);
+        AppWrapper.unmount();
+      });
+
+
+
+
+
+  });
