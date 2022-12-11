cc3d776083aff61a7b0ee5d07db9a39e645f5266
jchasehuskey <101059291+jchasehuskey@users.noreply.github.com>
2022-12-11 13:54:28 -0700

    edit

--- src/EventList.js
+++ src/EventList.js
@@ -4,9 +4,10 @@ import React, { Component } from "react";
  class EventList extends Component {
    render() {
      const { events } = this.props;
+
      return (
        <ul className="EventList">
-         {events.map((event) => (
+         {events?.map((event) => (
            <li key={event.id}>
              <Event event={event} />
            </li>
