ba0d0c7574e162ed25990df8e407cf4627b10c21
jchasehuskey <101059291+jchasehuskey@users.noreply.github.com>
2022-12-11 17:56:53 -0700

    updated getToken

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import './nprogress.css'
 import React,{Component} from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations } from './api';
+
 
 class App extends Component {
   state = {
